9f1fe1814e534c5a58880e006067455ab6db9319
ruslanfedoseenko <ruslan.fedoseenko.91@gmail.com>
2015-04-17 19:35:48 +0300

    Add missing files

---
+++ src/core/collections/ObservebaleCollectionBase.h
@@ -0,0 +1,16 @@
+﻿#ifndef _ObservebaleCollectionBase_INCLUDED_
+#define _ObservebaleCollectionBase_INCLUDED_
+#include <QObject>
+
+class ObservebaleCollectionBase : public QObject
+{
+	Q_OBJECT
+signals:
+	void itemAdeed(int index);
+	void itemsAdded(int startIndex, int count);
+	void itemRemoved(int index);
+	void itemsRemoved(int startIndex, int count);
+	void itemMoved(int from, int to);
+	void itemChanged(int index);
+};
+#endif
---
+++ src/core/helpers/VirtualDiskMounter.h
@@ -0,0 +1,22 @@
+﻿#ifndef _DiscMounter_INCLUDED_
+#define _DiscMounter_INCLUDED_
+
+
+#include <QString>
+#include <boost/smart_ptr/shared_ptr.hpp>
+
+class VirtualDiskMounter;
+typedef boost::shared_ptr<VirtualDiskMounter> VirtualDiskMounterPtr;
+class VirtualDiskMounter
+{
+#ifdef Q_WS_WIN
+	bool IsWin8OrGreater();
+	void MountVirualDiskImageWin8OrAbove(QString path);
+#endif
+	static boost::weak_ptr<VirtualDiskMounter> m_pInstance;
+public:
+	static VirtualDiskMounterPtr getInstance();
+	bool MountVirualDiskImage(QString path);
+};
+
+#endif
